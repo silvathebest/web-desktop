f3fe2fffe74cddbc066995945c95b51186b15bfb
silvathebest <silverchat228@gmail.com>
2020-01-19 00:16:19 +0400

    working db

--- js/db.js
+++
@@ -1,5 +0,0 @@
-var db = openDatabase("myDB", "1.0", "TiPS Database Example", 2*1024*1024);
-
-    db.transaction(function (tx) {
-        tx.executeSql("CREATE TABLE IF NOT EXISTS  myTable (id INTEGER PRIMARY KEY, nome TEXT, senha TEXT, email TEXT)");
-    })
